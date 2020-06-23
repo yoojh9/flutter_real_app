3c2ebc2c9192f553bb2f92ac0ef52742e7bcd7e3
Jeonghyun <yoojh9@gmail.com>
2020-06-23 17:49:33 +0900

    adding onChanged()

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import './transaction.dart';
- 
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -16,70 +16,93 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   final List<Transaction> transactions = [
-    Transaction(id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
-    Transaction(id: 't2', title: 'Weekly Grocery', amount: 16.53, date: DateTime.now()),
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Weekly Grocery', amount: 16.53, date: DateTime.now()),
   ];
+
+  String titleInput;
+  String amountInput;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
 
-      // card by default depends on the size of its child
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
-              color: Colors.blue,
-              child: Text('CHART'),
-              elevation: 5,),
-          ),
-          Card( 
-            elevation: 5,
-            child: Container(
-              child: Column(children: <Widget>[
-                TextField(),
-                TextField(),
-              ],
-          ),
-            ),),
-          Column(
-            children: transactions.map((tx) {
+        // card by default depends on the size of its child
+        body: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('CHART'),
+                elevation: 5,
+              ),
+            ),
+            Card(
+              elevation: 5,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      onChanged: (val) {
+                        titleInput = val;
+                      },
+                    ),
+                    TextField(
+                        decoration: InputDecoration(labelText: 'Amount'),
+                        onChanged: (val) => amountInput = val),
+                    FlatButton(
+                        textColor: Colors.purple,
+                        onPressed: () {
+                          print(titleInput);
+                          print(amountInput);
+                        },
+                        child: Text('add Transaction'))
+                  ],
+                ),
+              ),
+            ),
+            Column(
+                children: transactions.map((tx) {
               return Card(
-                child: Row(children: <Widget>[
+                  child: Row(
+                children: <Widget>[
                   Container(
                     margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                     decoration: BoxDecoration(
-                      border: Border.all(color: Colors.purple, width: 2,)),
+                        border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    )),
                     padding: EdgeInsets.all(10),
-                    child: Text(
-                      '\$${tx.amount}',
-                      style:TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.purple
-                      )
-                    ),
+                    child: Text('\$${tx.amount}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.purple)),
                   ),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                    Text(tx.title, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-                    Text(
-                      DateFormat.yMMMd().format(tx.date), 
-                      style:TextStyle(color: Colors.grey)
-                    ),
-                  ],)
+                      Text(tx.title,
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold)),
+                      Text(DateFormat.yMMMd().format(tx.date),
+                          style: TextStyle(color: Colors.grey)),
+                    ],
+                  )
                 ],
-                )
-              );
-          }).toList())
-        ],
-      ) 
-    );
+              ));
+            }).toList())
+          ],
+        ));
   }
 }
