79dfff9f33834eee57f7d88150cfd893eb1c556e
yoojh9 <yoojh9@gmail.com>
2020-06-24 14:16:29 +0900

    adding images to the App

--- lib/main.dart
+++ lib/main.dart
@@ -44,10 +44,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransaction = [
-    Transaction(
-        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
-    Transaction(
-        id: 't2', title: 'Weekly Grocery', amount: 16.53, date: DateTime.now()),
+    // Transaction(
+    //     id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    // Transaction(
+    //     id: 't2', title: 'Weekly Grocery', amount: 16.53, date: DateTime.now()),
   ];
 
   void _addNewTransaction(String txTitle, double txAmount) {
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,7 +11,20 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         height: 300,
-        child: ListView.builder(
+        child: transactions.isEmpty ? 
+          Column(children: <Widget>[
+            Text(
+              'No Transactions added yet!', 
+              style:Theme.of(context).textTheme.headline6
+            ),
+            SizedBox(height: 20,),
+            Container(
+              height: 200,
+              child: Image.asset('assets/images/waiting.png', 
+              fit:BoxFit.cover)
+            )
+          ],) 
+        : ListView.builder(
           itemBuilder: (ctx, index) {
             return Card(
                 child: Row(
