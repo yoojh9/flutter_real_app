9bc10152cc6afafab6e67198157207279738b2f9
yoojh9 <yoojh9@gmail.com>
2020-06-24 17:21:03 +0900

    add configuring widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -27,32 +27,22 @@ class TransactionList extends StatelessWidget {
         : ListView.builder(
           itemBuilder: (ctx, index) {
             return Card(
-                child: Row(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                    color: Theme.of(context).primaryColorLight,
-                    width: 2,
-                  )),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                      '\$${transactions[index].amount.toStringAsFixed(2)}',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, color: Theme.of(context).primaryColor)),
+              margin: EdgeInsets.symmetric(vertical:8, horizontal: 5),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius:30, 
+                    child: Padding(
+                      padding: EdgeInsets.all(6),
+                      child: FittedBox(child: Text('\$${transactions[index].amount}')),
+                    )
+                  ),
+                  title: Text(
+                    transactions[index].title, 
+                    style: Theme.of(context).textTheme.headline6
+                  ),
+                  subtitle: Text(DateFormat.yMMMd().format(transactions[index].date)),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(transactions[index].title,
-                        style: Theme.of(context).textTheme.headline6),
-                    Text(DateFormat.yMMMd().format(transactions[index].date),
-                        style: TextStyle(color: Colors.grey)),
-                  ],
-                )
-              ],
-            ));
+            );
           },
           itemCount: transactions.length,
         ));
